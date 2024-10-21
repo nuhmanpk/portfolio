bb0a3863568f38a4242da67093c416bb39307990
Nuhman Pk <62880706+nuhmanpk@users.noreply.github.com>
2024-10-21 17:51:19 +0530

    optim: add new certifications

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -241,6 +241,15 @@ export const RESUME_DATA = {
     }
   ], certifications: [
     {
+      title: " LangChain- Develop LLM powered applications with LangChain",
+      techStack: ["Udemy", "Oct 2024"],
+      description: "",
+      link: {
+        label: "udemy.com",
+        href: "https://www.udemy.com/certificate/UC-27ec2619-1f5e-4d21-9895-69a5dfaa9955/",
+      },
+    },
+    {
       title: "Pytorch for Deep Leaning",
       techStack: ["Udemy", "Feb 2023"],
       description: "",
@@ -301,6 +310,15 @@ export const RESUME_DATA = {
       link: {
         label: "https://www.cloudskillsboost.google/",
         href: "https://www.cloudskillsboost.google/public_profiles/350fa77e-4886-46b4-9d62-3910f47ba55c/badges/6643540",
+      },
+    },
+    {
+      title: "Image Captioning Models",
+      techStack: ["Google", "Aug 2024"],
+      description: "",
+      link: {
+        label: "https://www.cloudskillsboost.google/",
+        href: "https://www.cloudskillsboost.google/public_profiles/350fa77e-4886-46b4-9d62-3910f47ba55c/badges/10376858",
       },
     },
 
