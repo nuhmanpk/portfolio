842562dcd986b21ca2d9c75bf76402d64fe5fe06
Nuhman Pk <62880706+nuhmanpk@users.noreply.github.com>
2025-02-22 15:07:31 +0530

    Update resume-data.tsx

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -12,9 +12,9 @@ export const RESUME_DATA = {
   location: "Malappuram, Kerala",
   locationLink: "https://www.google.com/maps/place/Malappuram",
   about:
-    "24-Year-Old Senior Full Stack Engineer | Building Scalable Solutions with Passion (Yes, I'm 24. No, it's not a typo.)",
+    "Senior Full Stack Engineer, apparently at 24 | Building scalable solutions",
   summary:
-    "I'm a Senior Software Engineer and Full Stack MEARN Developer who loves open-source development, working with Python, and geeking out over AI & machine learning. I also have a soft spot for JavaScript (yes, I voluntarily chose this life). \n You’ll usually find me building cool stuff on GitHub and helping the developer community build things faster, and pretending I have everything under control in fast-paced environments. \n Oh, and I can do almost anything... until there exists a free version of  GPT",
+    "I'm Nuhman , a passionate open-source developer who loves working with Python, and geeking out over AI & machine learning. I also have a soft spot for JavaScript (yes, I voluntarily chose this life). \n You’ll usually find me building cool stuff on GitHub and helping the developer community build things faster, and pretending I have everything under control in fast-paced environments. \n Oh, and I can do almost anything... until there exists a free version of  GPT",
   personalWebsiteUrl: "https://nuhmanpk.github.io/portfolio",
   contact: {
     email: "nuhmanpk7@gmail.com",
