deaca728727b1d371c889646b4f9ee86bd363555
Nuhman Pk <62880706+nuhmanpk@users.noreply.github.com>
2025-10-23 17:31:22 +0530

    fix

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
-const repo = '/portfolio'; // replace with your repo name prefix ("/REPO_NAME") if different
-
 const nextConfig = {
   output: 'export',
-  basePath: repo,
-  assetPrefix: `${repo}/`,   // ensures assets are referenced under /portfolio/_next/...
-  images: {
-    unoptimized: true
-  },
+  images: { unoptimized: true }
 };
-
 module.exports = nextConfig;
