3cd43dbdb383f68060249555fda80139886cab55
Nuhman Pk <62880706+nuhmanpk@users.noreply.github.com>
2025-12-22 07:47:27 +0000

    chore: setup dev

--- src/data/education.tsx
+++
@@ -1,8 +0,0 @@
-export const education = [
-    {
-        school: "College of Applied Science",
-        degree: "Bachelor's Degree in Computer Science",
-        start: "2019",
-        end: "2022",
-    },
-];
